883ed972e753fd403c1d2fd276f140b55bd88c98
VadimOfficial <tester2terrorer@gmail.com>
2023-04-25 05:59:29 +0300

    fix: rotation

--- Assets/Scripts/CannonTower.cs
+++ Assets/Scripts/CannonTower.cs
@@ -5,46 +5,29 @@ public class CannonTower : TowerController
 {
 	[SerializeField] private GameObject m_muzzle;
 	[SerializeField] private float m_speedRotation = 0.5f;
-	// public override void Rotation(GameObject target)
-    // {
-    //     Vector3 targetPosition = target.transform.position;
-    //     Vector3 shooterPosition = m_shootPoint.transform.position;
-    //     //float distanceToTarget = Vector3.Distance(targetPosition, shooterPosition);
-    //     Vector3 displacement = targetPosition - shooterPosition;
-    //     float distanceToTarget = displacement.magnitude;
-    //     //Debug.Log(m_shootPoint.transform.position);
-    //     float timeToHit = distanceToTarget / m_speedProjectile;
-    //     Debug.Log(timeToHit);
-    //     Vector3 futureTargetPosition = targetPosition + target.GetComponent<Rigidbody>().velocity * timeToHit;
-    //     Vector3 directionToTarget = futureTargetPosition - shooterPosition;
-    //     Quaternion lookLocalRotation = Quaternion.LookRotation(directionToTarget);
-    //     float targetX = lookLocalRotation.x;
-    //     RotationMuzzle(targetX);
-    //     directionToTarget = new Vector3(directionToTarget.x, 0f, directionToTarget.z);
-    //     Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
-    //     transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * m_speedRotation);
-        
-    // }
-
-    // private void RotationMuzzle(float targetx)
-    // {
-    //     Quaternion targetRotation = Quaternion.Euler(targetx * 100f, 0f, 0f);
-    //     Quaternion currentRotation = m_muzzle.transform.localRotation;
-    //     Quaternion newRotation = Quaternion.Lerp(currentRotation, targetRotation, Time.deltaTime * m_speedRotation);
-    //     m_muzzle.transform.localRotation = newRotation;
-    // }
 
     public override void Rotation(GameObject target)
     {
         Vector3 targetDir = target.transform.position - m_shootPoint.transform.position;
         Vector3 targetVelocity = target.GetComponent<Rigidbody>().velocity;
-        float angle = Vector3.Angle(targetDir, transform.forward);
-        float timeToTarget = targetDir.magnitude / m_speedProjectile;
         float timeToIntersection = GetTimeToIntersection(targetDir, targetVelocity, m_speedProjectile);
         Vector3 aimPoint = target.transform.position + targetVelocity * timeToIntersection;
-        Debug.Log(aimPoint);
-        Vector3 aimDirection = aimPoint - transform.position;
-        transform.rotation = Quaternion.LookRotation(aimDirection);
+        Vector3 aimDirection = aimPoint - m_shootPoint.transform.position;
+        Quaternion lookLocalRotation = Quaternion.LookRotation(aimDirection);
+        float angleX = lookLocalRotation.x;
+        RotationMuzzle(angleX);
+        aimDirection = new Vector3(aimDirection.x, 0f, aimDirection.z);
+        Quaternion targetRotation = Quaternion.LookRotation(aimDirection);
+        Quaternion towerRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, m_speedRotation * Time.deltaTime);
+        transform.rotation = towerRotation;
+    }
+
+    private void RotationMuzzle(float angleX)
+    {
+        Quaternion targetRotation = Quaternion.Euler(angleX * 100f, 0f, 0f);
+        Quaternion currentRotation = m_muzzle.transform.localRotation;
+        Quaternion newRotation = Quaternion.Lerp(m_muzzle.transform.localRotation, targetRotation, Time.deltaTime * m_speedRotation);
+        m_muzzle.transform.localRotation = newRotation;
     }
 
     private float GetTimeToIntersection(Vector3 targetDir, Vector3 targetVelocity, float bulletSpeed)
@@ -58,8 +41,9 @@ public class CannonTower : TowerController
             return -1;
         }
 
-        float t1 = (-b + Mathf.Sqrt(discriminant)) / (2 * a);
-        float t2 = (-b - Mathf.Sqrt(discriminant)) / (2 * a);
+        float twoA = 2 * a;
+        float t1 = (-b + Mathf.Pow(discriminant, 0.5f)) / twoA;
+        float t2 = (-b - Mathf.Pow(discriminant, 0.5f)) / twoA;
 
         if (t1 < 0 && t2 < 0)
         {
