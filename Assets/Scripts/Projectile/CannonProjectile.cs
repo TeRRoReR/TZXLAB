a2db87fc6a48b39b9777845c29aeb5b198f10963
Vadim <v.kuzmin@xlab-game.com>
2023-04-30 04:49:54 +0300

    refactoring

--- Assets/Scripts/Projectile/CannonProjectile.cs
+++ Assets/Scripts/Projectile/CannonProjectile.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class CannonProjectile : Projectile , IProjectile
 {
+	private Rigidbody m_rb;
+
+	private void OnEnable()
+	{
+		m_rb = GetComponent<Rigidbody>();
+	}
+
 	protected override void Update(){}
 	
 	protected override void Move(Vector3 dir)
     {
-		Rigidbody rb = GetComponent<Rigidbody>();
-		rb.velocity = dir * m_speed;
+		m_rb.velocity = dir * m_speed;
 	}
 
 	public void Init(GameObject target, float speed, Vector3 dir)
